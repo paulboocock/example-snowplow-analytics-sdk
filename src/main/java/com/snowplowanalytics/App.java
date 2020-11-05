04a0190a51305acfa01cedf2f493d56968bf9a4a
Paul Boocock <paul@snowplowanalytics.com>
2020-11-05 22:12:46 +0000

    Switch to use noSpaces()

--- src/main/java/com/snowplowanalytics/App.java
+++ src/main/java/com/snowplowanalytics/App.java
@@ -6,8 +6,8 @@ import com.snowplowanalytics.snowplow.analytics.scalasdk.ParsingError;
 
 public class App {
 
-    private static String record = "{{MY-SITE-ID}}\tweb\t2020-10-23 15:59:59.197\t2020-10-23 15:59:58.994\t2020-10-23 15:59:58.979\tpage_view\t3b16e419-7f5b-4c12-9deb-02f5c455155f\t\tsp\tjs-2.16.2\tssc-1.0.0-kafka\tstream-enrich-1.4.0-common-1.4.0\t\t192.168.0.184\t\tfa5fe7d1-7276-4a5d-b1d5-65eecebe2721\t1\te01600c1-d336-474b-8968-048a0d5dacd5\t\t\t\t\t\t\t\t\t\t\t\thttp://my-site/\tmy-site-page-heading\thttp://my-site/\thttp\tmy-site\t80\t/\t\t\thttp\tmy-site\t80\t/\t\t\t\t\t\t\t\t\t\t\t{\"schema\":\"iglu:com.snowplowanalytics.snowplow/contexts/jsonschema/1-0-0\",\"data\":[{\"schema\":\"iglu:com.snowplowanalytics.snowplow/web_page/jsonschema/1-0-0\",\"data\":{\"id\":\"58548b84-318f-4c39-abfc-731440c5acd3\"}}]}\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tMozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0\t\t\t\t\t\ten-US\t\t\t\t\t\t\t\t\t\t1\t24\t1853\t974\t\t\t\tmy-tz\t\t\t1920\t1080\tUTF-8\t1853\t974\t\t\t\t\t\t\t\t\t\t\t\t2020-10-23 15:59:58.981\t\t\t\t2f878cfe-ccc6-4e93-a3fc-2bac0fd12857\t2020-10-23 15:59:58.992\tcom.snowplowanalytics.snowplow\tpage_view\tjsonschema\t1-0-0\t\t";
-    
+    private static final String record = "{{MY-SITE-ID}}\tweb\t2020-10-26 15:00:00.015\t2020-10-26 15:00:00.001\t2020-10-26 14:59:59.059\tpage_view\tafc17d66-cdd6-4f0b-a3c4-97521af63825\t\tsp\tjs-2.16.2\tssc-1.0.0-kafka\tstream-enrich-1.4.0-common-1.4.0\t\t192.168.0.2\t\t4c0ce0ea-66f6-4144-96f8-1155550ea2a5\t1\t136df257-4267-4af1-8338-552f95e5b352\t\t\t\t\t\t\t\t\t\t\t\thttps://snowplowanalytics.com/\tSnowplow Analytics\thttps://snowplowanalytics.com/\thttps\tsnowplowanalytics.com\t80\t/\t\t\thttps\tsnowplowanalytics.com\t80\t/\t\t\t\t\t\t\t\t\t\t\t{\"schema\":\"iglu:com.snowplowanalytics.snowplow/contexts/jsonschema/1-0-0\",\"data\":[{\"schema\":\"iglu:com.snowplowanalytics.snowplow/web_page/jsonschema/1-0-0\",\"data\":{\"id\":\"ff5fa637-3c07-42d9-9516-2aea4b15c60c\"}}]}\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tMozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:81.0) Gecko/20100101 Firefox/81.0\t\t\t\t\t\ten-US\t\t\t\t\t\t\t\t\t\t1\t24\t1853\t974\t\t\t\tmy-tz\t\t\t1920\t1080\tUTF-8\t1853\t974\t\t\t\t\t\t\t\t\t\t\t\t2020-10-26 15:00:00.000\t\t\t\t538339ca-3900-444d-af20-3b01e219ad01\t2020-10-26 15:00:00.010\tcom.snowplowanalytics.snowplow\tpage_view\tjsonschema\t1-0-0\t\t";
+  
 	public static void main(String[] args) {
 
         // Convert Enriched TSV String using Scala Analytics SDK
@@ -20,7 +20,7 @@ public class App {
             Event ev = validatedEvent.toOption().get();
 
             // Convert to JSON String if desired
-            String jsonString = ev.toJson(false).toString(); // Or true if you want lossy output
+            String jsonString = ev.toJson(false).noSpaces(); // Or true if you want lossy output
 
             // Get individual properties of the event
             System.out.println(ev.platform().get());
